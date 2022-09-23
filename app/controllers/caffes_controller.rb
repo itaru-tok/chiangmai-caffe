7e7ef37a72a59716a70fb76633a97cf9e087ff8e
itaruby_on_rails <98925848+itaru-tokuda@users.noreply.github.com>
2022-09-23 18:13:40 +0700

    define show/index/new/create method, create each template files

--- app/controllers/caffes_controller.rb
+++ app/controllers/caffes_controller.rb
@@ -1,2 +1,30 @@
 class CaffesController < ApplicationController
+
+  def show
+    @caffe = Caffe.find(params[:id])
+  end
+
+  def index
+    @caffes = Caffe.all
+  end
+
+  def new
+    @caffe = Caffe.new
+  end
+
+  def create
+    @caffe = Caffe.new(refer_params)
+    if @caffe.save
+      flash[:notice] = "Caffe was successfully created"
+      redirect_to caffes_path
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+  def refer_params
+    params.require(:caffe).permit(:name, :comment, :image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "home#index"
+  root "caffes#index"
   resources :caffes
 end
